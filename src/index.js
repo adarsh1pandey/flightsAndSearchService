ab91a90303befb92bfa6b6ac6baf7743aae8b822
Adarsh <adarshpnd71@gmail.com>
2023-10-07 21:51:56 +0530

    feat: setup dotenv to the project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const {PORT} = require("./config/serverconfig");
+
+
+const setupAndStartServer = async () => {
+    const app = express();
+    app.listen(PORT, () => {
+        console.log(`server started at PORT number ${PORT}`)
+    })
+}
+
+setupAndStartServer();
