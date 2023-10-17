de071b2eb71b78014eec5ecb47db77544fe10532
Adarsh <adarshpnd71@gmail.com>
2023-10-17 23:52:05 +0530

    feat: added unique constraints and started city repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const {PORT} = require("./config/serverconfig");
+const {City} = require('./models/index')
+const CityRepository  = require('./repository/city-repository')
 
 
 const setupAndStartServer = async () => {
@@ -10,8 +12,14 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`server started at PORT number ${PORT}`)
+        // await City.create({
+        //     name: "New Delhi",
+        // });
+        const repo = new CityRepository();
+        repo.createCity({name: "Varanasi"});
+        console.log(City, "this is City guys")
     })
 }
 
