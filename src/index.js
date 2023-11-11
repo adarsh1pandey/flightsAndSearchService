8060f4a556db2c994651c5eb1eff531c9d98eb64
Adarsh <adarshpnd71@gmail.com>
2023-11-12 01:03:19 +0530

    added filter to get all cities

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 const {PORT} = require("./config/serverconfig");
 const ApiRoutes = require('./routes/index');
 
+const db = require('./models/index')
+
+
+const {Airport, City} = require('./models/index') 
+
 const setupAndStartServer = async () => {
 
     const app = express();
@@ -11,8 +16,27 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use('/api', ApiRoutes  )
 
-    app.listen(3000 ,() => {
-        console.log(`server started at PORT number 3000`)
+    app.listen(3000 ,async () => {
+        console.log(`server started at PORT number 3000`);
+        // const airports = await Airport.findAll({include:[{ model: City}]});
+        // console.log(airports, "this is all airports");
+        // const airports = await City.findAll({
+        //     where: {
+        //         id: 9
+        //     }, 
+        //     include: [{
+        //         model: Airport
+        //     }]
+        // })
+
+        // const result = db.sequelize.sync({alter: true});
+
+        const city = await City.findOne({
+            where: {
+                id: 30,
+            }
+        })
+        console.log(city, "this is city guys ")
     })
 }
 
